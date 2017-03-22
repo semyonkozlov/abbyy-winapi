8e232dafaca9682d3f2e6d5d14aac0e4f25c1a13
Semyon Kozlov <semkozloff@gmail.com>
2017-03-22 23:33:30 +0300

    Add dll and lib creating examples.

---
+++ WordsCountDllTest2/Main.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <fstream>
+#include <iterator>
+#include <functional>
+
+#include <Windows.h>
+
+const wchar_t* LibPath = L"C:/Users/Semyon/Documents/Visual Studio 2017/Projects/AbbyyWinapi/x64/Debug/WordsCountDll.dll";
+
+int main( int argc, char* argv[] )
+{
+    HMODULE wordsCountDll = LoadLibrary( LibPath );
+    auto wordsCount = reinterpret_cast<int (*)( const wchar_t* )>( GetProcAddress( wordsCountDll, "WordsCount" ) ); 
+
+    int argCount;
+    LPWSTR* argVector = CommandLineToArgvW( GetCommandLine(), &argCount );
+    if( argCount > 1 ) {
+        std::wifstream file( argVector[1] );
+        std::wstring fileContent( std::istreambuf_iterator<wchar_t>( file ), {} );
+        int numWords = wordsCount( fileContent.c_str() );
+        MessageBox( NULL, std::to_wstring( numWords ).c_str(), L"WordsCount", MB_DEFAULT_DESKTOP_ONLY );
+    }
+
+    return EXIT_SUCCESS;
+}
---
+++ WordsCountLib/WordsCount.h
@@ -0,0 +1,4 @@
+#pragma once
+
+int WordsCount( const wchar_t* text );
+bool IsDelimiter( wchar_t symbol );
