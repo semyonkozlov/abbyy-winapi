2dc891c67584643d63e1882feba59f35fa86dc4c
Semyon Kozlov <semkozloff@gmail.com>
2017-06-15 19:50:02 +0300

    Remove DllExample

--- DllExample/WordsCountDll/WordsCount.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#ifndef WORDS_COUNT_API
-#define WORDS_COUNT_API extern "C" __declspec( dllimport )
-#endif
-
-WORDS_COUNT_API int WordsCount( const wchar_t* text );
-WORDS_COUNT_API bool IsDelimiter( wchar_t symbol );
--- DllExample/WordsCountLib/WordsCount.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-int WordsCount( const wchar_t* text );
-bool IsDelimiter( wchar_t symbol );
